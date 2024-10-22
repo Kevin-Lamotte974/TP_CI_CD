e6fa1e1625bf6c2ce34b9afb64c76e33788c72e7
Kevin Lamotte <84518512+Kevin-Lamotte974@users.noreply.github.com>
2024-10-22 17:31:19 +0200

    Update sum.js

--- sum.js
+++ sum.js
@@ -2,6 +2,6 @@ function sum(...numbers) {
     return numbers.reduce((acc, current) => acc + current, 0);
 }
 
-//  Version dev -> main test 4
+//  Version dev -> main test 5
 
 module.exports = sum;
